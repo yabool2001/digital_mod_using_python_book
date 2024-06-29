a2e52940c6f906d1f7381c22a0a8fa8caa82c522
yabool2001 <mzemlo.pl@gmail.com>
2024-06-28 20:25:49 -0400

    aaaa

--- dsp/signal_gen.py
+++ dsp/signal_gen.py
@@ -1,23 +1,29 @@
-def sine_wave_gen ( f , f_s , osr , phase_shift , nCyl ) :
+def sine_wave_gen ( sin , a , f , f_s , N , p_s , osr , depict ) :
     """
     Generate sine wave signal with the following parameters
     Parameters:
     f : frequency of sine wave in Hertz
-    osr : oversampling rate (integer)
-    phase_shift : desired phase_shift shift in radians
-    nCyl : number of cycles of sine wave to generate
+
+    osr         oversampling rate (integer) for plot
+    p_s         desired phase shift in radians
+    N :         number of
+    depict      plot the signal
     Returns:
-    (t,g) : time base (t) and the signal g(t) as tuple
-    Example:
-    f=10; osr=30;
-    phase_shift = 1/3*np.pi ; nCyl = 5;
-    (t,g) = sine_wave(f,osr,phase_shift,nCyl)
+    ( t , g )   time base (t) and the signal g(t) as tuple
     """
-
-    import numpy as np
     
-    f_os = osr * f_s # sampling frequency
-    t = np.arange ( 0 , nCyl * 1 / f - 1 / f_s , 1 / f_s ) # time base
-    g = np.sin ( 2 * np.pi * f * t + phase_shift ) # replace with cos if a cosine wave is desired
+    import numpy as np
 
-    return ( t , g ) # return time base and signal g(t) as tuple
+    t_s = 1.0 / f_s  # Okres próbkowania
+    t_n = np.arange ( 0 , N * t_s , t_s ) # Wyznaczenie punktów próbkowania t_n
+
+    if ( sin ) :
+        x_n = np.sin ( a * np.pi * f * t_n + p_s )
+    else :
+        x_n = np.cos ( a * np.pi * f * t_n + p_s )
+    
+    if ( depict ) :
+        import matplotlib.pyplot as plt
+        
+
+    return ( t_n , x_n ) # return time base and signal as tuple
