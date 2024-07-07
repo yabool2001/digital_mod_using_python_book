9c430227107767ad60ea775cdfb5d999885ced7f
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2024-07-08 00:32:04 +0200

    aaaa

--- demo_001.py
+++ demo_001.py
@@ -8,9 +8,9 @@ sys.path.append ( os.path.join ( 'C:/Users/mzeml/python/digital_mod_using_python
 import dsp.gen_signal as gen_signal
 from dsp.plot_signal import plot_signal # import function
 
-( n , x_n ) = gen_signal.wave ( 1 , 2 , 2000.0 , 8000.0 , 8 , 0 , 10 , 1 ) #fi phase = 1 / 3 * np.pi #phase shift in radians
-plot_signal ( n , x_n )
+# ( n , x_n ) = gen_signal.wave ( 1 , 2 , 2000.0 , 8000.0 , 8 , 0 , 10 , 1 ) #fi phase = 1 / 3 * np.pi #phase shift in radians
+# plot_signal ( n , x_n )
 # ( n , x_n ) = gen_signal.rect_pulse ( 1 , 10 , 9 , 3 , 0 , 0 , 0 ) #fi phase = 1 / 3 * np.pi #phase shift in radians
 # plot_signal ( n , x_n )
-# ( t , x_t ) = gen_signal.chirp ( 1 , 10 , 9 , 3 , 0 , 0 , 0 ) #fi phase = 1 / 3 * np.pi #phase shift in radians
-# plot_signal ( n , x_n )
+( t , x_t ) = gen_signal.chirp ( 1 , 1500 , 1000 , 10 , 50 , 0 , 'linear' , True , 1 ) #fi phase = 1 / 3 * np.pi #phase shift in radians
+plot_signal ( t , x_t )
--- dsp/gen_signal.py
+++ dsp/gen_signal.py
@@ -1,4 +1,4 @@
-def wave ( sin , a , f , f_s , N , p_s , osr , depict ) :
+def wave ( sin , a , f , f_s , N , p_o , osr , depict ) :
     """
     Generate sine wave signal with the following parameters
     Parameters:
@@ -7,7 +7,7 @@ def wave ( sin , a , f , f_s , N , p_s , osr , depict ) :
     f           frequency of wave in Hertz
     f_s         sampling frequency
     N           the number of samples of the input sequence and the number of frequency points in the DFT output.
-    p_s         desired phase shift in radians
+    p_o         desired phase offset in radians
     osr         oversampling rate (integer) used for plot
     depict      plot the signal
     Returns:
@@ -21,17 +21,17 @@ def wave ( sin , a , f , f_s , N , p_s , osr , depict ) :
     print ( f"{t=}" )
 
     if ( sin ) :
-        x_n = np.sin ( a * np.pi * f * t + p_s )
+        x_n = np.sin ( a * np.pi * f * t + p_o )
     else :
-        x_n = np.cos ( a * np.pi * f * t + p_s )
+        x_n = np.cos ( a * np.pi * f * t + p_o )
     
     if ( depict ) :
         import matplotlib.pyplot as plt
         l = np.linspace ( 0 , ( N - 1 ) * t_s , num = N * osr ) # Na rysunku powinno być więcej punktów niż do analizy
         if ( sin ) :
-            x_t = np.sin ( a * np.pi * f * l + p_s )
+            x_t = np.sin ( a * np.pi * f * l + p_o )
         else :
-            x_t = np.cos ( a * np.pi * f * l + p_s )
+            x_t = np.cos ( a * np.pi * f * l + p_o )
         plt.figure ( figsize = ( 10 , 5 ) )
         plt.plot ( l , x_t , label = 'x_t amplitude' )
         plt.scatter ( t , x_n , color = 'red' , marker = '*' , s = 100 , label = 'Samples (t)' )
@@ -91,7 +91,7 @@ def rect_pulse ( a , f_s , N , K , s , sym , depict ) :
 
     return ( t , x_t ) # return time base and signal as tuple
 
-def chirp ( a , f_s , N , K , s , depict ) :
+def chirp ( a , f_s , N , f_0 , f_1 , p_o , m , v_z , depict ) :
     """
     A general rectangular function x ( n ) can be defined as N samples containing K unity-valued samples.
     Parameters:
@@ -112,7 +112,16 @@ def chirp ( a , f_s , N , K , s , depict ) :
     from scipy.signal import chirp
     
     t_s = 1.0 / f_s  # sampling period
+    t_1 = ( N - 1 ) * t_s
     t = np.arange ( 0 , N * t_s , t_s ) # Sampling points generation
     print ( f"{t=}" )
-    t = np.arange ( 0, 1 , step = 1/fs ) #total time base from 0 to 1 second
-    g = chirp(t, f0=1, t1=0.5, f1=20, phi=0, method='linear')
+    print ( f"{t=} {f_s=} {f_0=} {t_1=} {f_1=} {p_o=} {m=} {p_o=} {v_z=}" )
+    # x_t = chirp ( t , f_0 , t_1 , f_1 , m , p_o )
+    # t =np.arange(start = 0, stop = 1,step = 1/500)
+    x_t = chirp ( t , f_0 , t1 = t_1 , f1 = f_1 , phi = p_o , method = m , vertex_zero = v_z )
+    if depict :
+        import matplotlib.pyplot as plt
+        plt.plot ( t , x_t )
+        plt.show ()
+    
+    return ( t , x_t ) # return time base and signal as tuple
